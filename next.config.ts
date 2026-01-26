6d7d046f8e05e126ccf2e0ea3a4a43fc6dd7eefc
Bence Dobos <dobosbence@gmail.com>
2026-01-26 23:27:08 +0000

    github pages wants to create a nextjs.yml into .github/workflows/ direct

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/studio',
+  assetPrefix: '/studio/',
   typescript: {
     ignoreBuildErrors: true,
   },
