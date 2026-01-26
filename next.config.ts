5127856a1448135cda13dc6b1bc7cea328d5dcf9
Bence Dobos <dobosbence@gmail.com>
2026-01-26 23:29:40 +0000

    now I got 404 error in Firebase Studio's preview

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/studio',
-  assetPrefix: '/studio/',
   typescript: {
     ignoreBuildErrors: true,
   },
